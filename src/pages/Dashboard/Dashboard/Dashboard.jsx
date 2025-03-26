8c09e3634bf65066a4e53302b42a2129bb81925b
Patricia Gracia Gutierrez <pgg.uxui@gmail.com>
2025-03-26 20:39:33 +0000

    Improved AI implementation design

--- src/pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,167 +1,73 @@
 import { Link, useLocation, Outlet } from "react-router-dom";
 import { AuthContext } from "../../../context/auth.context";
 import { useContext, useState } from "react";
+import "./Dashboard.css";
 
 function Dashboard() {
-  const location = useLocation(); // Para detectar la ruta activa
+  const location = useLocation(); 
   const { user } = useContext(AuthContext);
   const [menuOpen, setMenuOpen] = useState(false);
 
-  // Función para determinar si un enlace está activo
-  const isActive = (path) => {
-    return location.pathname === `/dashboard${path}`;
-  };
+  const isActive = (path) => location.pathname === `/dashboard${path}`;
 
   return (
     <div className="min-h-screen bg-gray-100 pt-16">
-      {/* Menú de edición */}
       <div className="bg-white shadow-md pt-4">
         <div className="container mx-auto px-4 py-4">
           {/* Versión desktop */}
           <nav className="hidden md:flex justify-between items-center px-8">
             <div className="flex space-x-8">
-              <Link
-                to="/dashboard/info"
-                className={`text-lg font-semibold ${isActive("/info")
-                  ? "text-indigo-500 border-b-2 border-indigo-500"
-                  : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"
-                  } transition-colors`}
-              >
+              <Link to="/dashboard/info" className={`text-lg font-semibold ${isActive("/info") ? "text-indigo-500 border-b-2 border-indigo-500" : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"} transition-colors`}>
                 Info
               </Link>
-              <Link
-                to="/dashboard/curriculum"
-                className={`text-lg font-semibold ${isActive("/curriculum")
-                  ? "text-indigo-500 border-b-2 border-indigo-500"
-                  : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"
-                  } transition-colors`}
-              >
+              <Link to="/dashboard/curriculum" className={`text-lg font-semibold ${isActive("/curriculum") ? "text-indigo-500 border-b-2 border-indigo-500" : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"} transition-colors`}>
                 Curriculum
               </Link>
-              <Link
-                to="/dashboard/projects"
-                className={`text-lg font-semibold ${isActive("/projects")
-                  ? "text-indigo-500 border-b-2 border-indigo-500"
-                  : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"
-                  } transition-colors`}
-              >
+              <Link to="/dashboard/projects" className={`text-lg font-semibold ${isActive("/projects") ? "text-indigo-500 border-b-2 border-indigo-500" : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"} transition-colors`}>
                 Proyectos
               </Link>
-              <Link
-                to="/dashboard/contact"
-                className={`text-lg font-semibold ${isActive("/contact")
-                  ? "text-indigo-500 border-b-2 border-indigo-500"
-                  : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"
-                  } transition-colors`}
-              >
+              <Link to="/dashboard/contact" className={`text-lg font-semibold ${isActive("/contact") ? "text-indigo-500 border-b-2 border-indigo-500" : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"} transition-colors`}>
                 Contacto
               </Link>
-              <Link
-                to="/dashboard/seo"
-                className={`text-lg font-semibold ${isActive("/seo")
-                  ? "text-indigo-500 border-b-2 border-indigo-500"
-                  : "text-gray-700 hover:text-indigo-500 hover:border-b-2 hover:border-indigo-500"
-                  } transition-colors`}
-              >
-                SEO Analysis
+            </div>
+
+            {/* Contenedor de botones SEO y Preview */}
+            <div className="flex space-x-4">
+              <Link to={`/paginaprofesional/${user?._id}`} title="Haz clic para ver el diseño de tu curriculum" target="_blank" rel="noopener noreferrer" className={`text-lg font-semibold px-4 py-2 rounded-lg ${isActive("/preview") ? "bg-indigo-600 text-white" : "bg-indigo-100 text-indigo-500 hover:bg-indigo-200"} transition-colors`}>
+                Preview
+              </Link>
+              <Link to="/dashboard/seo" title="Analiza tu perfil con IA en búsqueda de palabras clave" className="text-lg font-semibold px-4 py-2 rounded-lg bg-whit border-2 border-indigo-500 animate-glow hover:bg-yellow-50 hover:border-indigo-700 glowing-border">
+                ✨
               </Link>
             </div>
-            <Link
-              to={`/paginaprofesional/${user?._id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`text-lg font-semibold px-4 py-2 rounded-lg ${isActive("/preview")
-                ? "bg-indigo-600 text-white"
-                : "bg-indigo-100 text-indigo-500 hover:bg-indigo-200"
-                } transition-colors`}
-            >
-              Preview
-            </Link>
           </nav>
 
           {/* Versión móvil */}
           <div className="md:hidden">
             <div className="flex justify-between items-center">
-              {/* Nuevo icono para el menú de secciones */}
-              <button
-                onClick={() => setMenuOpen(!menuOpen)}
-                className="text-gray-700 focus:outline-none"
-              >
+              <button onClick={() => setMenuOpen(!menuOpen)} className="text-gray-700 focus:outline-none">
                 <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-                  {menuOpen ? (
-                    <path d="M6 18L18 6M6 6l12 12" />
-                  ) : (
-                    <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-                  )}
+                  {menuOpen ? <path d="M6 18L18 6M6 6l12 12" /> : <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />}
                 </svg>
               </button>
-              <Link
-                to="/dashboard/seo"
-                className={`block py-2 px-2 rounded ${isActive("/seo")
-                  ? "bg-indigo-50 text-indigo-500"
-                  : "text-gray-700 hover:bg-gray-50"
-                  }`}
-                onClick={() => setMenuOpen(false)}
-              >
-                SEO Analysis
-              </Link>
-              {/* Botón de Preview siempre visible */}
-              <Link
-                to={`/paginaprofesional/${user?._id}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`text-sm font-semibold px-3 py-1 rounded-lg ${isActive("/preview")
-                  ? "bg-indigo-600 text-white"
-                  : "bg-indigo-100 text-indigo-500 hover:bg-indigo-200"
-                  } transition-colors`}
-              >
-                Preview
-              </Link>
+
+              <div className="flex space-x-2">
+                <Link to={`/paginaprofesional/${user?._id}`} target="_blank" rel="noopener noreferrer" className={`text-sm font-semibold px-3 py-1 rounded-lg ${isActive("/preview") ? "bg-indigo-600 text-white" : "bg-indigo-100 text-indigo-500 hover:bg-indigo-200"} transition-colors`}>
+                  Preview
+                </Link>
+                <Link to="/dashboard/seo" className="block text-sm font-semibold px-3 py-1 rounded-lg bg-white border-2 border-indigo-500 hover:bg-yellow-50 hover:border-indigo-700 glowing-border">
+                ✨
+                </Link>
+              </div>
             </div>
 
             {/* Menú desplegable móvil */}
             {menuOpen && (
               <div className="mt-4 space-y-2 pb-2">
-                <Link
-                  to="/dashboard/info"
-                  className={`block py-2 px-2 rounded ${isActive("/info")
-                    ? "bg-indigo-50 text-indigo-500"
-                    : "text-gray-700 hover:bg-gray-50"
-                    }`}
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Info
-                </Link>
-                <Link
-                  to="/dashboard/curriculum"
-                  className={`block py-2 px-2 rounded ${isActive("/curriculum")
-                    ? "bg-indigo-50 text-indigo-500"
-                    : "text-gray-700 hover:bg-gray-50"
-                    }`}
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Curriculum
-                </Link>
-                <Link
-                  to="/dashboard/projects"
-                  className={`block py-2 px-2 rounded ${isActive("/projects")
-                    ? "bg-indigo-50 text-indigo-500"
-                    : "text-gray-700 hover:bg-gray-50"
-                    }`}
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Proyectos
-                </Link>
-                <Link
-                  to="/dashboard/contact"
-                  className={`block py-2 px-2 rounded ${isActive("/contact")
-                    ? "bg-indigo-50 text-indigo-500"
-                    : "text-gray-700 hover:bg-gray-50"
-                    }`}
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Contacto
-                </Link>
+                <Link to="/dashboard/info" className={`block py-2 px-2 rounded ${isActive("/info") ? "bg-indigo-50 text-indigo-500" : "text-gray-700 hover:bg-gray-50"}`} onClick={() => setMenuOpen(false)}>Info</Link>
+                <Link to="/dashboard/curriculum" className={`block py-2 px-2 rounded ${isActive("/curriculum") ? "bg-indigo-50 text-indigo-500" : "text-gray-700 hover:bg-gray-50"}`} onClick={() => setMenuOpen(false)}>Curriculum</Link>
+                <Link to="/dashboard/projects" className={`block py-2 px-2 rounded ${isActive("/projects") ? "bg-indigo-50 text-indigo-500" : "text-gray-700 hover:bg-gray-50"}`} onClick={() => setMenuOpen(false)}>Proyectos</Link>
+                <Link to="/dashboard/contact" className={`block py-2 px-2 rounded ${isActive("/contact") ? "bg-indigo-50 text-indigo-500" : "text-gray-700 hover:bg-gray-50"}`} onClick={() => setMenuOpen(false)}>Contacto</Link>
               </div>
             )}
           </div>
@@ -170,7 +76,7 @@ function Dashboard() {
 
       {/* Contenido principal */}
       <main className="container mx-auto px-4 sm:px-6 py-8">
-        <Outlet /> {/* Aquí se renderizan las rutas anidadas */}
+        <Outlet />
       </main>
     </div>
   );
--- src/pages/Dashboard/SeoAnalysis/SeoAnalysis.jsx
+++ src/pages/Dashboard/SeoAnalysis/SeoAnalysis.jsx
@@ -21,26 +21,35 @@ function SeoAnalysis() {
         };
 
         if (user) fetchSeoAnalysis();
-    }, [user]); 
+    }, [user]);
 
-    // Función para capitalizar la primera letra de cada palabra
-    const capitalize = (text) => {
-        return text.replace(/\b\w/g, (char) => char.toUpperCase());
+    // Función para capitalizar solo la primera letra de toda la frase
+    const capitalizeFirstLetter = (text) => {
+        if (!text) return '';
+        return text.toLowerCase()
+                  .charAt(0).toUpperCase() + text.toLowerCase().slice(1);
     };
 
-    // Convertir el string en un array y capitalizar las palabras
+    // Convertir el string en un array y formatear cada palabra clave
     const keywordsArray = seoData?.suggestions
         ?.split(",")
-        ?.map(word => capitalize(word.trim())) || [];
+        ?.map(word => capitalizeFirstLetter(word.trim())) || [];
 
     return (
         <div className="bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold text-gray-800">SEO Analysis</h2>
+            <h2 className="text-2xl font-semibold text-indigo-500 text-center">Análisis de tu perfil con la IA Gemini</h2>
             {loading && <p className="text-gray-500">Analizando...</p>}
+            
+            {!seoData && !loading && (
+                <p className="text-red-600 text-left font-medium mt-4">
+                    Rellena los campos "Breve descripción", "Profesión" y "Resumen profesional" para que la Inteligencia Artificial analice tu perfil.
+                </p>
+            )}
+
             {seoData && (
-                <div className="mt-4">
-                    <p className="text-black">
-                        <strong>Hemos analizado tu perfil y estas son las palabras clave que deberías incluir en tu currículum y perfil profesional para mejorar su visibilidad en buscadores como Google y facilitar que más personas te encuentren. Incorporar estas palabras ayudará a destacar tu experiencia y habilidades:</strong>
+                <div className="mt-4 text-left">
+                    <p className="text-black font-medium mb-8">
+                        Estas son las palabras clave que deberías incluir en tu perfil profesional para mejorar tu visibilidad en buscadores como Google, o redes sociales como Linkedin, y facilitar que más personas te encuentren. Incorporar estas palabras ayudará a destacar tu experiencia y habilidades:
                     </p>
                     <ul className="list-disc list-inside mt-2 text-black">
                         {keywordsArray.map((keyword, index) => (
